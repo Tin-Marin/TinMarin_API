fbcd0d1606aeba809aa53bf7071e1f388726b57f
kaztro <00229017@uca.edu.sv>
2022-06-22 00:18:57 -0600

    Corrección pequeña en el router.delete de sound

--- routes/api/v1-private/Sounds.js
+++ routes/api/v1-private/Sounds.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const SoundsController = require('./../../../controllers/api/private/Sounds');
-const { route } = require('./Suggestion');
 
 router.get('/', SoundsController.findAll);
-router.delete('/:id', SoundsController.remove);
+router.delete('/:_id', SoundsController.remove);
 
 module.exports = router;
